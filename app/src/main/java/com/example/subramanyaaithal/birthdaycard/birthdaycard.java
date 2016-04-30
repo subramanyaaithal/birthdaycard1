bc2164be4dcf05870c070cb6320dcdc5653da3e3
Subramanya Aithal <subrahmanya1@gmail.com>
2016-04-30 14:37:05 +0530

    Initial commit for birthday card application for android

---
+++ app/src/main/java/com/example/subramanyaaithal/birthdaycard/birthdaycard.java
@@ -0,0 +1,13 @@
+package com.example.subramanyaaithal.birthdaycard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class birthdaycard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_birthdaycard);
+    }
+}
